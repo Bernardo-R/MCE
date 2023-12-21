7d88559bfa18183a3ea83802246be649bf82df76
Bernardo-R <brosario12@live.com>
2023-12-20 21:34:21 -0500

    update firebaseConfig

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -6,12 +6,12 @@ export default defineNuxtPlugin((nuxtApp) => {
     const runtimeConfig = useRuntimeConfig()
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCELWs_mD6SATTp-fIpyB-AW25IUI59mtU",
-    authDomain: "marycargoexpress-a92f9.firebaseapp.com",
-    projectId: "marycargoexpress-a92f9",
-    storageBucket: "marycargoexpress-a92f9.appspot.com",
-    messagingSenderId: "192337920653",
-    appId: "1:192337920653:web:46bcea586399af4986d8c4"
+    apiKey : runtimeConfig.firebase.apiKey,
+    authDomain : runtimeConfig.firebase.authDomain,
+    projectId : runtimeConfig.firebase.projectId,
+    storageBucket : runtimeConfig.firebase.storageBucket,
+    messagingSenderId : runtimeConfig.firebase.messagingSenderId,
+    appId : runtimeConfig.firebase.appId
   };
 
   const app = initializeApp(firebaseConfig)
